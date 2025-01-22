7bd45c96b7740993b6f1fae4c34ca8762da38b45
Sambhav <sambhav12wakhariya@gmail.com>
2025-01-22 23:42:20 +0530

    Checked In : Some Patterns Questions

---
+++ Intermeddiate 100 Question JS/Patterns.js
@@ -0,0 +1,52 @@
+// SQUARE PATTERN
+
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//     for(let j = 0; j < n; j++){
+//         process.stdout.write(" * ");
+//     }
+//     console.log();
+// }
+
+// HOLLOW SQUARE PATTERN
+
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//     for(let j = 0; j < n; j++){
+//         if(i == 0 || i == n-1 || j == 0 || j == n-1) process.stdout.write(" * ");
+//         else process.stdout.write("   ");
+//     }
+//     console.log();
+// }
+
+// HOLLOW INVERTED PATTERN
+
+// let n = 6;
+
+// for(let i = 0; i < n; i++){
+//     for(let j = 0; j < n; j++){
+//         if(i == 0 || j == 0 || j ==  (n-i-1)) process.stdout.write(" * ");
+//         else process.stdout.write("   ");
+//     }
+//     console.log();
+// }
+
+// HOLLOW FULL PYRAMID
+// let n = 5;
+// for(let i = 0; i < n; i++){
+//     let k = 0;
+//     for(let j= 0; j < 2*n-1; j++){
+//         if(j < n-i-1) process.stdout.write("   ");
+//         else if(k < 2*i+1) {
+//             process.stdout.write(" * ");
+//             k++;
+//         }else process.stdout.write("   ");
+//     }
+//     console.log();
+// }
+
+
+
+
