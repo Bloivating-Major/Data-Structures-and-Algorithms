5b7920003f6d90cb10617de3ad6046c2aeeeaae3
Sambhav <sambhav12wakhariya@gmail.com>
2025-02-13 23:48:03 +0530

    Checked In : Maps and Sets Concepts

---
+++ Concepts/Maps and Sets/concept.js
@@ -0,0 +1,55 @@
+// Sets
+
+// const set = new Set();
+// console.log(set);
+
+// set.add(1);
+// set.add(2);
+// set.add("Sam");
+
+
+// console.log(set);
+
+// for(let elem of set){
+//     console.log(elem);
+// }
+
+// console.log(set.has("Sam"));
+// console.log(set.has("sam"));
+
+// set.delete(2);
+
+// console.log(set);
+
+// console.log(set.size);
+
+// set.clear();
+
+// console.log(set);
+
+// Maps
+
+// const map = new Map();
+
+// console.log(map);
+
+// map.set('firstName', 'Sambhav'); // adds a key value pair to map. 
+// map.set(1, "Sambhav");
+// map.set("Sambhav", "Sam");
+
+// console.log(map);
+
+// console.log(map.get("Sambhav")); // finds the element
+
+// console.log(map.has(1));
+
+// map.delete(1);
+
+// console.log(map.keys())
+
+// console.log(map);
+
+// for( let [key, value] of map){
+//     console.log(key, value);
+// }
+
